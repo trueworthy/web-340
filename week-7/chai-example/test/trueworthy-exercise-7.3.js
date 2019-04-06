15ea93397b44f9692857076e8dd3becbef17adea
Lea Trueworthy <ltrueworthy@my365.bellevue.edu>
2019-04-06 13:12:06 -0700

    fixing exercise 7.3

--- week-7/chai-example/test/trueworthy-exercise-7.3.js
+++ week-7/chai-example/test/trueworthy-exercise-7.3.js
@@ -1,9 +1,26 @@
 /*
 ============================================
-; Title:  Chai example
+; Title:  trueworthy-exercise-7.3.js
 ; Author: Professor Krasso 
 ; Date:   4 April 2019
 ; Modified By: Lea Trueworthy
 ; Description: Exercise 7.3 - Chai-example
 ;===========================================
 */
+
+function fruits(str) {
+    return str.split(',');
+}
+
+module.exports = fruits;
+
+//Unit Test
+var fruits = require("../trueworthy-fruit");
+var chai = require("chai");
+var assert = chai.assert;
+describe("fruits", function () {
+    it("should return an array of fruits", function () {
+        var f = fruits('Apple,Orange,Mango');
+        assert(Array.isArray(f));
+    });
+});
---
+++ week-7/chai-example/trueworthy-fruit.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  trueworthy-fruit.js
+; Author: Professor Krasso
+; Date:   5 April 2019
+; Modified By: Lea Trueworthy
+; Description: Function used in a Chai test.
+;===========================================
+*/
+
+function fruits(str) {
+    return str.split(",");
+}
+
+module.exports = fruits;
